1af99c5e1a66ada0e96ac1f173807152b83544ea
Serkanydn <serkanydn96@gmail.com>
2022-11-06 23:16:34 +0300

    Uploading the first files

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const port = 3000
+
+//ejs template engine
+app.set("view engine", "ejs")
+
+//static files middleware
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/about', (req, res) => { 
+    res.render('about');
+})
+
+
+app.listen(port, () => [
+    console.log(`Application listening on port:${port}`)
+])
