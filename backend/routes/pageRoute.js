39b99979a9232683801bf8f9b276051a44fbca9b
Serkanydn <serkanydn96@gmail.com>
2022-11-09 23:11:05 +0300

    sendMail added

--- backend/routes/pageRoute.js
+++ backend/routes/pageRoute.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout } = require('../controllers/pageController')
+const { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout, getContactPage,sendMail } = require('../controllers/pageController')
 const { auth } = require('../utils');
 
 router.route('/').get(getIndexPage);
@@ -7,7 +7,9 @@ router.route('/about').get(getAboutPage);
 router.route('/register').get(getRegisterPage);
 router.route('/login').get(getLoginPage);
 router.route('/logout').get(getLogout);
+router.route('/contact').get(getContactPage);
+router.route('/contact').post(sendMail);
 
 
-
+ 
 module.exports = router
